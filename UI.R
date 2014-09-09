a621a94ce5a61d452e175812a0b30aa0dabcf24a
timelyportfolio <kent.russell@timelyportfolio.com>
2014-09-09 17:41:58 -0500

    add tree stylesheet as demonstration

--- UI.R
+++ UI.R
@@ -1,12 +1,21 @@
 library(rCharts)
 
-shinyUI(fluidPage(
-  mainPanel(
-    sliderInput("slider1", label = h3("Min Split"),
-                min = 1, max = 10, value = 4),
-    tableOutput("test"),
-    tableOutput("rpk.text"),
-    showOutput("plot", "dimple"))
-            
-#  showOutput("plot", "dimple")
-))
+shinyUI(
+  tagList(
+    tags$head(
+      tags$link(
+        href = "http://timelyportfolio.github.io/rCharts_rpart/css/treestyle.css"
+        ,rel = "stylesheet"
+      )
+    ),
+    fluidPage(
+      
+      mainPanel(
+        sliderInput("slider1", label = h3("Min Split"),
+                    min = 1, max = 10, value = 4),
+        tableOutput("test"),
+        tableOutput("rpk.text"),
+        showOutput("plot", "dimple"))
+    )
+  )
+)
